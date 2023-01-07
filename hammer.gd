9ada05c35a33e354147a3894d615a86e956e1bef
Yumepix <yumepix@pm.me>
2023-01-07 16:32:20 +0100

    feat: background + hammer animation

---
+++ hammer.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var _hammer_animation = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	
+	if (Input.is_action_just_pressed("crush")):
+		_hammer_animation.play("crush_anim")
+
+		
