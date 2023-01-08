1458b98c00f129aa04edefb155e4f1ce7843884f
Yumepix <yumepix@pm.me>
2023-01-08 17:38:49 +0100

    feat: add farmer and carotte

--- global.gd
+++ global.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var tab_of_holes_taken = []
+var tab_of_carottes_taken = []
 
 func play_crush()-> void:
 	$snd_Crush.play()
--- main.gd
+++ main.gd
@@ -4,13 +4,17 @@ extends Node2D
 #const nb_cols = 8
 #const nb_lines = 7
 @onready var tab_of_holes = []
+@onready var tab_of_carottes = []
 @onready var begin_grid = Vector2.ZERO
 @onready var background = $Background
 @onready var hammer = $Hammer
 
-var nb_ennemies = 0
-var Mole = preload("res://taupe.tscn")
 
+var nb_ennemies = 0
+var nb_farmers = 0
+var Mole = preload("res://taupe.tscn")
+var Carotte = preload("res://carotte.tscn")
+var Farmers = preload("res://farmer.tscn")
 
 func _ready() -> void:
 	# cache le curseur
@@ -23,10 +27,13 @@ func _ready() -> void:
 		var tile = background.get_cell_atlas_coords(0,cell)
 		if tile == Vector2i(5,1):
 			tab_of_holes.append(cell)
+		if tile == Vector2i(2,2) || tile == Vector2i(5,2) || tile == Vector2i(5,3):
+			tab_of_carottes.append(cell)
 	
 	tab_of_holes.shuffle()
-	print(tab_of_holes)
-	print(background.map_to_local(Vector2(4,3)))
+	pop_carotte()
+
+
 	
 	
 
@@ -49,11 +56,18 @@ func _physics_process(delta: float) -> void:
 
 	hammer.position = pos_mouse
 	nb_ennemies = get_tree().get_nodes_in_group("ennemies").size();
+	nb_farmers = get_tree().get_nodes_in_group("farmers").size();
 #	print(nb_ennemies)
 #	print(Global.tab_of_holes_taken)
 	pop_mole()
+	pop_farmer()
 
 
+func pop_carotte() -> void:
+	for c in tab_of_carottes:
+		var carotte = Carotte.instantiate()
+		carotte.pop(background.map_to_local(c))
+		add_child(carotte)
 
 func pop_mole() -> void:
 	if (nb_ennemies < 4):	
@@ -67,7 +81,20 @@ func pop_mole() -> void:
 			Global.tab_of_holes_taken.append(hole)
 		else:
 			print("hole taken")
-			
+
+func pop_farmer() -> void:
+	if (nb_farmers < 4):	
+		var rand_index:int = randi() % tab_of_holes.size()
+		var hole = tab_of_holes[rand_index]
+		if !is_hole_taken(hole):
+			var farmer = Farmers.instantiate()
+			farmer.pop(background.map_to_local(hole))
+			add_child(farmer)
+			farmer.hole = hole
+			Global.tab_of_holes_taken.append(hole)
+		else:
+			print("hole taken")
+
 
 func is_hole_taken(hole: Vector2i):
 	for h in Global.tab_of_holes_taken:
